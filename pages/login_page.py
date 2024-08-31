5fd48e5b9a30704190949aa4231aa075ecf599cf
Matheus Caldas <raimundo_caldas@terceiros.sicredi.com.br>
2024-08-31 08:46:31 -0300

    :recycle: fix: Refatoração no teste de login

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,3 +1,5 @@
+# pages/login_page.py
+
 from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
 
@@ -5,6 +7,7 @@ class LoginPage(BasePage):
     USERNAME_INPUT = (By.ID, "user-name")
     PASSWORD_INPUT = (By.ID, "password")
     LOGIN_BUTTON = (By.ID, "login-button")
+    ERROR_MESSAGE = (By.XPATH, "//div[@class='error-message-container error']")
     PRODUCTS_TITLE = (By.CLASS_NAME, "title")
 
     def __init__(self, driver):
@@ -15,6 +18,9 @@ class LoginPage(BasePage):
         self.enter_text(self.USERNAME_INPUT, username)
         self.enter_text(self.PASSWORD_INPUT, password)
         self.click(self.LOGIN_BUTTON)
+
+    def get_error_message(self):
+        return self.find(self.ERROR_MESSAGE).text
 
     def is_login_successful(self, expected_title):
         self.wait_for_title(expected_title)
--- tests/login_test.py
+++ tests/login_test.py
@@ -1,6 +1,14 @@
+import os
+import json
+from dotenv import load_dotenv
 import pytest
 from selenium import webdriver
 from pages.login_page import LoginPage
+
+load_dotenv()
+
+credentials_json = os.getenv("LOGIN_CREDENTIALS")
+credentials = json.loads(credentials_json)
 
 @pytest.fixture
 def driver():
@@ -8,20 +16,30 @@ def driver():
     yield driver
     driver.quit()
 
-def test_login(driver):
-    USERNAME = "standard_user"
-    PASSWORD = "secret_sauce"
+def test_login_success(driver):
     EXPECTED_TITLE = "Swag Labs"
+    user = credentials['login_success']
 
     login_page = LoginPage(driver)
+    login_page.login(user['username'], user['password'])
 
-    login_page.login(USERNAME, PASSWORD)
+    assert login_page.is_login_successful(EXPECTED_TITLE)
+    assert login_page.is_on_products_page()
 
-    assert login_page.is_login_successful(EXPECTED_TITLE), f"O título esperado era '{EXPECTED_TITLE}', e o título encontrado foi '{driver.title}'"
+def test_login_failure_incorrect_password(driver):
+    EXPECTED_ERROR_MESSAGE = "Epic sadface: Username and password do not match any user in this service"
+    user = credentials['login_failure_incorrect_password']
 
-    assert login_page.is_on_products_page(), f"O texto esperado era 'Products', e o texto encontrado foi diferente"
+    login_page = LoginPage(driver)
+    login_page.login(user['username'], user['password'])
 
-    print(f"Login bem-sucedido. Título da página: {driver.title}")
+    assert login_page.get_error_message() == EXPECTED_ERROR_MESSAGE
 
-if __name__ == "__main__":
-    pytest.main(["-v"])
+def test_login_failure_locked_out_user(driver):
+    EXPECTED_ERROR_MESSAGE = "Epic sadface: Sorry, this user has been locked out."
+    user = credentials['test_login_failure_locked_out_user']
+
+    login_page = LoginPage(driver)
+    login_page.login(user['username'], user['password'])
+
+    assert login_page.get_error_message() == EXPECTED_ERROR_MESSAGE
